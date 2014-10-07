9610a4c290217c6ee46ea4fbe72488ccbd8ddac6
Stefanie Contreras <stefsnow@gmail.com>
2014-10-06 20:18:00 -0700

    possibly fix wiki not found error on server

--- server/robot.js
+++ server/robot.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 7085;
 
-var wiki = require('./wikiParser.js')
+var wiki = require('./wikiParser')
 
 // app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
